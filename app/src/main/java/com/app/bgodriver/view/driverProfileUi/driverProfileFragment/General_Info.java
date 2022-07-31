777e4e235c7578dcbde6f8fbdcc26612b4df35bf
User <shawon.mas@gmail.com>
2022-07-31 19:17:30 +0600

    :white_check_mark: adding map requirements

--- app/src/main/java/com/app/bgodriver/view/driverProfileUi/driverProfileFragment/General_Info.java
+++ app/src/main/java/com/app/bgodriver/view/driverProfileUi/driverProfileFragment/General_Info.java
@@ -1,10 +1,20 @@
 package com.app.bgodriver.view.driverProfileUi.driverProfileFragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,49 +23,79 @@ import android.widget.Toast;
 
 import com.app.bgodriver.databinding.FragmentGeneralInfoBinding;
 import com.app.bgodriver.model.FragmentToActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class General_Info extends Fragment {
 
    private FragmentGeneralInfoBinding binding;
-    private FragmentToActivity mCallback;
+    private FusedLocationProviderClient client;
 
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            mCallback = (FragmentToActivity) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement FragmentToActivity");
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding=FragmentGeneralInfoBinding.inflate(inflater, container, false);
+        client= LocationServices.getFusedLocationProviderClient(getActivity());
+        clickListeners();
+
 
         return binding.getRoot();
     }
-    @Override
-    public void onDetach() {
-        mCallback = null;
-        super.onDetach();
+
+    private void clickListeners() {
+          binding.generalInfoLocationMap.setOnClickListener(v -> {
+              checkPermission();
+
+          });
     }
 
-    public void onRefresh() {
-        Toast.makeText(getActivity(), "Fragment : Refresh called.",
-                Toast.LENGTH_SHORT).show();
-    }
-    private void sendDta(String text,String text2) {
-        mCallback.communicate(text,text2);
+    private void checkPermission() {
+        Dexter.withContext(getActivity()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                getCurrentLocation();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent=new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri=Uri.fromParts("package",getActivity().getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+
+            }
+        });
 
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+
+
+
     }
+
+
 }
